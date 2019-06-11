811563f9ebebeedc25e98858758742c151f2d8bd
Victor Reinor <victorreinor@gmail.com>
2019-06-11 20:28:40 -0300

    adicionar postcontroller para poder inserir posts com imagens e buscar os posts no banco de dados utilizando a tipagem feita na models

---
+++ src/models/Post.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const PostScheme = new mongoose.Schema({
+  author: String,
+  place: String,
+  description: String,
+  hashtags: String,
+  image: String,
+  likes: {
+    type: Number,
+    default: 0,
+  },
+}, {
+  timestamps: true,
+});
+
+module.exports = mongoose.model('Post', PostScheme);
